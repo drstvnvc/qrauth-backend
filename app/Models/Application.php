8cc56d48a2c81f39c31a35ca2daf9868e4372caf
Drago Stevanovic <42371846+drstvnvc@users.noreply.github.com>
2020-06-18 19:09:32 +0200

    Create application model and migration (#4)

---
+++ app/Models/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    protected $fillable = [ 'application_id', 'application_secret', 'homepage_url' ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'application_secret',
+    ];
+}
---
+++ database/migrations/2020_06_18_160528_create_applications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('app_id')->unique();
+            $table->string('app_secret');
+            $table->string('homepage_url')->nullable(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
